1522d2b7e70af68cde5be36b2480d525b3d54dc9
csdiaries <159630328+csdiaries@users.noreply.github.com>
2024-02-10 15:54:23 +0200

    fix formatting

--- easy/two-number-sum.go
+++ easy/two-number-sum.go
@@ -4,18 +4,17 @@ import "sort"
 
 func TwoNumberSum(array []int, target int) []int {
 	sort.Ints(array)
-    left := 0
-    right := len(array) - 1
-    for left <= right {
-		currentSum := array[left] + array[right];
-        if currentSum == target {
-            return []int{array[left], array[right]}
-        } else if currentSum < target {
-            left += 1
-        } else {
-            right -= 1
-        }
+	left := 0
+	right := len(array) - 1
+	for left <= right {
+		currentSum := array[left] + array[right]
+		if currentSum == target {
+			return []int{array[left], array[right]}
+		} else if currentSum < target {
+			left += 1
+		} else {
+			right -= 1
+		}
 	}
 	return []int{}
 }
-
