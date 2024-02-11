de5d16f8d3b1d6057d5439dba142c230de905b88
csdiaries <159630328+csdiaries@users.noreply.github.com>
2024-02-11 09:06:00 +0200

    added validate subsequence

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Easy
 🟢 [Two Number Sum](https://www.algoexpert.io/questions/two-number-sum) - [solution](https://github.com/csdiaries/algoexpert/blob/main/easy/two-number-sum.go)
+🟢 [Validate Subsequence](https://www.algoexpert.io/questions/validate-subsequence) - [solution](https://github.com/csdiaries/algoexpert/blob/main/easy/validate-subsequence.go)
---
+++ easy/validate-subsequence.go
@@ -0,0 +1,20 @@
+package main
+
+func IsValidSubsequence(array []int, sequence []int) bool {
+	if len(sequence) > len(array) {
+		return false
+	}
+
+    var pointer int
+    for _, num := range array {
+        if pointer == len(sequence) {
+            break
+        }
+        
+        if num == sequence[pointer] {
+            pointer += 1
+        }
+    }
+
+    return pointer == len(sequence)
+}
